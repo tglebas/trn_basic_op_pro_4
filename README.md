631be9a2add577d7250f17eb3ed83318bf555e9c
Tom Glebas <tom.glebas@videoray.com>
2017-06-05 16:24:46 -0400

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+trn_basic_op
+============
+
+Basic Operator Training
---
+++ javascript/intro_about.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+//VideoRay
+if (document.images) {
+   hand = new Image;
+   hand1 = new Image;
+   hand2 = new Image;
+
+   hand.src = '../../required/images/icon_hand_full.png';
+   hand1.src = '../../required/images/icon_hand_full.png';
+   hand2.src = '../../required/images/icon_hand_full_blue.png';
+   }
+</script>
